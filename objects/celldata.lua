3807e559c5f950bf45caf9487182a5a8a79f3b90
guizarolho <guimfontes7@gmail.com>
2026-09-15 16:17:43 -0300

    extends cell data, now records char

--- objects/celldata.lua
+++ objects/celldata.lua
@@ -1,8 +1,9 @@
 local CellData = Object:extend()
 
-function CellData:new(isObstacle, isCollectable)
+function CellData:new(isObstacle, isCollectable, utf8char)
     self.obstacle = isObstacle
     self.collectable = isCollectable
+    self.char = utf8char
 end
 
 return CellData
