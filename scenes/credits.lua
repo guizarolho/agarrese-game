67286c482fec93129b0ed8fead6a49102b46be29
Flavio Eduardo <flavioedu40@gmail.com>
2026-09-17 10:15:22 -0300

    mudanca nas telas de menu e credito

--- scenes/credits.lua
+++ scenes/credits.lua
@@ -1,9 +1,13 @@
 Credits = Object:extend()
 
+--[[
 function Credits:new()
-    -- self.image = path
+    -- Add background image
+    self.background = love.graphics.newImage("assets/credits_background.png")
+
     love.graphics.setFont(FONT)
 end
+]]
 
 function Credits:update(dt)
 end
@@ -13,11 +17,45 @@ function Credits:keypressed(key)
 end
 
 function Credits:draw()
-    -- love.graphics.newImage(self.image)
-    love.graphics.print(
-        "Thank you for playing!",
-        50,
-        420
+    local screenWidth = love.graphics.getWidth()
+    local screenHeight = love.graphics.getHeight()
+
+    --[[
+    local imageWidth = self.background:getWidth()
+    local imageHeight = self.background:getHeight()
+
+    -- Calculate scale to fill the entire screen
+    local scale = math.max(
+        screenWidth / imageWidth,
+        screenHeight / imageHeight
+    )
+
+    -- Draw background centered and scaled
+    love.graphics.draw(
+        self.background,
+        screenWidth / 2,
+        screenHeight / 2,
+        0,
+        scale,
+        scale,
+        imageWidth / 2,
+        imageHeight / 2
+    )
+    ]]
+
+    -- Message
+    local text = 
+    
+    "Obrigado por jogar!\n\n".. 
+    "ADICIONAR OS NOMES DO GRUPO\n\n" ..
+
+
+    love.graphics.printf(
+        text,
+        0,
+        screenHeight / 2 - FONT:getHeight() / 2,
+        screenWidth,
+        "center"
     )
 end
 
