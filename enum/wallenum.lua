7cc62a55f57cbb4c717c1aa1d698fd007319d6b2
guizarolho <guimfontes7@gmail.com>
2026-09-17 21:02:31 -0300

    adds tile map for level 3

--- enum/wallenum.lua
+++ enum/wallenum.lua
@@ -1,7 +1,7 @@
 local WallEnum = {
     [1] = {"sprites/wall_level1.png", "sprites/wall_level1_alt.png"},
     [2] = {"sprites/wall_level2.png", "sprites/wall_level2_alt.png"},
-    [3] = {"sprites/wall_level1.png", "sprites/wall_level1_alt.png"},
+    [3] = {"sprites/wall_level3.png", "sprites/wall_level3_alt.png"},
     [4] = {"sprites/wall_level1.png", "sprites/wall_level1_alt.png"}
 }
 
